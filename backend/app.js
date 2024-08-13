efdd447555ae58e407238a17fe386912ab36334a
Bhuvan <bhuvanshyam2022@gmail.com>
2024-08-13 18:25:18 +0530

    setup the express server

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./routes/auth');
+const employeeRouter = require('./routes/employee');
 
-var users = require('./routes/users');
+const app = express();
 
-var app = express();
+mongoose.connect('mongodb://localhost:27017/employee-management', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(express.json());
+app.use('/api/auth', authRouter);
+app.use('/api/employees', employeeRouter);
 
-app.use('/api/v1/users', users);
-
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
